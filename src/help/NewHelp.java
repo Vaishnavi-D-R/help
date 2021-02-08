bd87bd3d647c3b7496a42de32445d61ef68b8d49
Vaishnavi-D-R <vaishnavidr2000@gmail.com>
2021-02-08 10:59:34 +0530

    first commit

---
+++ src/help/NewHelp.java
@@ -0,0 +1,10 @@
+package help;
+
+public class NewHelp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hiii");
+	}
+
+}
---
+++ src/help/SecondHelp.java
@@ -0,0 +1,10 @@
+package help;
+
+public class SecondHelp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
